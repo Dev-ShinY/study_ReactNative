917084edfb08a00aafdf19b4a97c6fd63ce3412c
shinyunsung <114058540+Dev-ShinY@users.noreply.github.com>
2022-11-18 17:37:31 +0900

    :sparkles: Add to get weather

--- App.tsx
+++ App.tsx
@@ -1,35 +1,53 @@
 import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
-import { View, Text, Dimensions, StyleSheet, ScrollView, Alert } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = process.env.APIKEY_WEATHER;
 
 export default function App() {
-  const [location, setLocation] = useState<string | null>();
+  const [city, setCite] = useState<string | null>();
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     try {
+      // get locaiton
       await Location.requestForegroundPermissionsAsync();
       const {
-        coords: {latitude, longitude}
-      } = await Location.getCurrentPositionAsync({accuracy: 5});
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
       const location = await Location.reverseGeocodeAsync(
-        {latitude, longitude},
-        {useGoogleMaps: false}
-      )      
-      setLocation(location[0].city);
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setCite(location[0].city);
+
+      // get weather
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+      );
+      const json = await response.json();
+      setDays(json.daily);
     } catch (error) {
-      setOk(false)
-      Alert.alert('놉.') 
+      setOk(false);
+      Alert.alert("놉.");
     }
   };
   useEffect(() => {
-    ask();
+    getWeather();
   });
 
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>{location}</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
 
       <ScrollView
@@ -38,25 +56,42 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map(
+            (
+              day: {
+                temp: {
+                  day: string;
+                };
+                weather: [
+                  {
+                    main: string;
+                    description: string;
+                  }
+                ];
+              },
+              index
+            ) => (
+              <View key={index} style={styles.day}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Text style={styles.description}>{day.weather[0].main}</Text>
+                <Text style={styles.tinyText}>
+                  {day.weather[0].description}
+                </Text>
+              </View>
+            )
+          )
+        )}
       </ScrollView>
     </View>
   );
@@ -85,6 +120,9 @@ const styles = StyleSheet.create({
     marginTop: 50,
     fontWeight: "600",
     fontSize: 178,
+  },
+  tinyText: {
+    fontSize: 20,
   },
   description: {
     marginTop: -30,
